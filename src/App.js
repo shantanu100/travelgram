e8476f6b4168526e3c73beadb2df9c01219ce5a6
Shantanu Kulkarni <shantanukulkarni100@gmail.com>
2024-12-15 10:48:27 +0530

    added ui for login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './modules/Login';
 
 function App() {
   return (
     <div className="App">
-      <h1>Basic Setup </h1>
+      <Login />
     </div>
   );
 }
